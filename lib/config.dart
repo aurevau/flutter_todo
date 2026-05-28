a273b995834f01822e70368d321c29c1b85243b9
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-05-28 08:47:44 +0200

    updated backend URL

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
 final url = 'http://localhost:3000/';
-final registration = url + "registration";
-final login_url = url + "login";
+final registration = "${url}registration";
+final login_url = "${url}login";
