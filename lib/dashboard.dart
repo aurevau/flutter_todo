7cb5e7f032fd5da1e30819c84444b9f1835df2b1
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-05-29 21:16:33 +0200

    Created dialogbox for creating a todo in dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:todo_application/widgets/custom_background.dart';
+import 'package:todo_application/widgets/custom_button.dart';
+import 'package:todo_application/widgets/custom_text_field.dart';
+import 'package:todo_application/widgets/custom_text_field_label.dart';
 
 class Dashboard extends StatefulWidget {
   final String token;
+
   const Dashboard({super.key, required this.token});
 
   @override
@@ -11,6 +16,15 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   String email = '';
+  TextEditingController _todoTitle = TextEditingController();
+  TextEditingController _todoDescription = TextEditingController();
+
+  final Color _primaryColor = const Color(0xFF4F46E5);
+  final Color _darkTextColor = const Color(0xFF0F172A);
+  final Color _bodyTextColor = const Color(0xFF475569);
+  final Color _borderColor = const Color(0xFFE2E8F0);
+  final Color _bgLight = const Color(0xFFF8FAFC);
+
   @override
   void initState() {
     super.initState();
@@ -27,6 +41,83 @@ class _DashboardState extends State<Dashboard> {
           children: [Text(email)],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _displayTextInputDialog(context),
+        child: Icon(Icons.add),
+        tooltip: "Add todo",
+      ),
+    );
+  }
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: SizedBox(
+            width: 280,
+            height: 300,
+            child: Stack(
+              children: [
+                CustomBackground(
+                  color: _primaryColor,
+                  topPosition: -60,
+                  leftPosition: -60,
+                  rightPosition: -60,
+                  bottomPosition: -80,
+                  upperBlobSize: 280,
+                  lowerBlobSize: 230,
+                ),
+                SafeArea(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 20),
+                        CustomTextFieldLabel(
+                          text: "Add Todo",
+                          bodyColor: _darkTextColor,
+                          fontSize: 16,
+                        ),
+                        SizedBox(height: 20),
+                        CustomTextField(
+                          bodyColor: _bodyTextColor,
+                          borderColor: _borderColor,
+                          controller: _todoTitle,
+                          darkColor: _darkTextColor,
+                          icon: Icons.title,
+                          hint: "Title",
+                          primaryColor: _primaryColor,
+                        ),
+                        SizedBox(height: 12),
+                        CustomTextField(
+                          bodyColor: _bodyTextColor,
+                          borderColor: _borderColor,
+                          controller: _todoDescription,
+                          darkColor: _darkTextColor,
+                          icon: Icons.description,
+                          hint: "Description",
+                          primaryColor: _primaryColor,
+                        ),
+
+                        SizedBox(height: 20),
+
+                        CustomButton(
+                          btnColor: _darkTextColor,
+                          buttonText: "Save",
+                          onPressedButton: () {
+                            // addTodo();
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
